2a233f28ddd78a78a8c0f45c86977c5763729d07
Stéphane TRÉBEL (Perso) <stephane.trebel@gmail.com>
2025-06-17 16:02:04 +0200

    Allow mouse in Hardtime (I only use it to browse help popups anyway)

--- nvim/lua/enable_hardtime.lua
+++ nvim/lua/enable_hardtime.lua
@@ -4,6 +4,7 @@ vim.notify = require("notify")
 -- Break bad habits, master Vim motions
 -- See plugins lua
 require("hardtime").setup({
+    disable_mouse = false,
     -- I keep arrow keys because HJKL makes no sense with my layout
     disabled_keys = {
       ["<Up>"] = {},
